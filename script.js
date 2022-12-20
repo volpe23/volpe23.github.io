09873b994e0da2734ebbb6c294e03d8991d20fb2
volpe23 <arozmianecs@gmail.com>
2022-12-20 13:41:49 +0200

    Added semicolons to JS code

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const choices = ['rock', 'paper', 'scissors']
+const choices = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)].toLowerCase()
+    return choices[Math.floor(Math.random() * 3)].toLowerCase();
 }
 
 function playerChoice() {
     player = prompt("What do you chose?").toLowerCase()
-    if (choices.includes(player)) return player
-    else return console.error("Wrong input!") 
+    if (choices.includes(player)) return player;
+    else return console.error("Wrong input!");
 }
 
 console.log(getComputerChoice())
 function playRound(playerSelection, computerSelection) {
     const match = choices.indexOf(playerSelection) - choices.indexOf(computerSelection)
     if (match === 0) {
-        console.log("Tie!")
-        return 2
+        console.log("Tie!");
+        return 2;
     }
     let winner = (match === -2 || match === 1) ? true : false;
     if (winner) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
-        return true
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        return true;
     }
     else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
-        return false
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return false;
     }
 }
 
@@ -33,19 +33,19 @@ function game() {
     let computerScore = 0;
     for (let i = 0; i < 5; i++) {
         console.log(`Round ${i + 1}`)
-        const computer = getComputerChoice()
-        const player = playerChoice()
-        const winner = playRound(player, computer) 
+        const computer = getComputerChoice();
+        const player = playerChoice();
+        const winner = playRound(player, computer);
         if (winner === 2) {
-            console.log(`Current score - ${playerScore} : ${computerScore}`)
-            continue
+            console.log(`Current score - ${playerScore} : ${computerScore}`);
+            continue;
         }
-        winner ? playerScore++ : computerScore++
-        console.log(`Current score - ${playerScore} : ${computerScore}`)
+        winner ? playerScore++ : computerScore++;
+        console.log(`Current score - ${playerScore} : ${computerScore}`);
     }
-    const score = playerScore - computerScore 
-    if (score > 0) console.log("You win the game!")
-    else if (score === 0) console.log("It\'s a tie!") 
-    else console.log("You lose! Computer wins!")
+    const score = playerScore - computerScore ;
+    if (score > 0) console.log("You win the game!");
+    else if (score === 0) console.log("It\'s a tie!");
+    else console.log("You lose! Computer wins!");
     
 }
