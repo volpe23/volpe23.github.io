c54041c884b68e60837c0f4abd1acc293adb26d4
volpe23 <arozmianecs@gmail.com>
2022-12-21 20:33:13 +0200

    Refactored game

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const choices = ['rock', 'paper', 'scissors'];
 
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)].toLowerCase();
 }
@@ -8,52 +12,64 @@ function playerChoice(value) {
     if (choices.includes(value)) return value;
     else return console.error("Wrong input!");
 }
+
+
+
 function playRound(playerSelection, computerSelection) {
     const match = choices.indexOf(playerSelection) - choices.indexOf(computerSelection)
     if (match === 0) {
         console.log("Tie!");
-        return 2;
+        // return 2;
     }
     let winner = (match === -2 || match === 1) ? true : false;
     if (winner) {
         console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return true;
+        console.log(`Current score - ${playerScore} : ${computerScore}`);
+        playerScore++;
+        // return true;
     }
     else {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        return false;
+        console.log(`Current score - ${playerScore} : ${computerScore}`);
+        computerScore++;
+        // return false;
+    }
+    updateScore();
+    if (isWinner()) {
+        start.classList.remove("show-hide");
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}`)
-        const computer = getComputerChoice();
-        const player = playerChoice();
-        const winner = playRound(player, computer);
-        if (winner === 2) {
-            console.log(`Current score - ${playerScore} : ${computerScore}`);
-            continue;
-        }
-        winner ? playerScore++ : computerScore++;
-        console.log(`Current score - ${playerScore} : ${computerScore}`);
-    }
-    const score = playerScore - computerScore ;
-    if (score > 0) console.log("You win the game!");
-    else if (score === 0) console.log("It\'s a tie!");
-    else console.log("You lose! Computer wins!");
+function newGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScore()
+    start.classList.add("show-hide");
+}
+const playerScoreEl = document.querySelector("#player-score");
+const computerScoreEl = document.querySelector("#computer-score");
+
+
+function updateScore() {
+    playerScoreEl.textContent = `${playerScore}`;
+    computerScoreEl.textContent = `${computerScore}`;
+    
+    
+}
+function isWinner() {
+    console.log("We have winner")
+    return playerScore >= 5 || computerScore >= 5 ? true : false
 }
 
 function eventsOnButtons() {
     buttons = document.querySelectorAll(".btn");
-    start = document.querySelector("#start");
-    start.addEventListener('click', () => game())
     buttons.forEach(el => {
         el.addEventListener('click', () => {
-            playRound(el.value, getComputerChoice())
+            isWinner() ?  console.log("Start new game") : playRound(el.value, getComputerChoice())
         })
     });
 }
+start = document.querySelector("#start");
+start.addEventListener('click', newGame)
 eventsOnButtons()
+newGame()
